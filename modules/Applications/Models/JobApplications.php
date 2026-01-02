3c560f4a11bcd6b7e4e2ee43e4aa7fd3c0638418
coder-niraj <niraj.coder@gmail.com>
2026-01-02 14:58:49 +0530

    this

---
+++ modules/Applications/Models/JobApplications.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class JobApplications extends Model
+{
+    public $incrementing = false;      // disable auto increment
+    protected $keyType = 'string';     // key type is string (UUID)
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                // Generate UUID when creating model
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use App\Livewire\Settings\TwoFactor;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+require base_path('modules/Admin/Routes/web.php');
